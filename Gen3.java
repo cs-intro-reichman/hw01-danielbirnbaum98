ff95d9a2e8f8f81d9ab2b7902cc60c411b93b974
danielbirnbaum98 <154251785+danielbirnbaum98@users.noreply.github.com>
2023-12-20 18:42:34 +0200

    Add files via upload

---
+++ Gen3.java
@@ -0,0 +1,35 @@
+public class Gen3 {
+    public static void main(String[] args) {
+        
+
+        int num_1 = Integer.parseInt(args[0]);
+        int num_2 = Integer.parseInt(args[1]);
+
+        int min = Math.min(num_1 , num_2);
+        int max = Math.max(num_1 , num_2);
+
+
+        int a = (int)(Math.random() * (max - min) + min);
+        int b = (int)(Math.random() * (max - min) + min);
+        int c = (int)(Math.random() * (max - min) + min);
+
+        int min_1 = Math.min(a , b);
+        int min_2 = Math.min(b , c);
+        int min_total = Math.min(min_1 , min_2);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("The minimal generated number was " + min_total);
+        
+
+
+        
+
+
+    
+        
+    
+
+    }
+}
--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,24 @@
-/*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
- */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+        
+
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
+        boolean result = false;
+        
+
+        if((x+y)>z && (x+z)>y && (z+y)>x){
+            result = true;
+        }
+
+
+
+       
+
+        System.out.println(x + ", " + y + ", " + z + ": " + result);
+        
+    
+
+    }
 }
